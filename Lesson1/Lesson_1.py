e686fa10632b2b030c119178d011cdae9d18369f
Kabrel <37102421+Kabrel@users.noreply.github.com>
2021-04-15 22:00:43 +0300

    Lesson 1 End
    Lesson 1 End

---
+++ Lesson1/Lesson_1.py
@@ -0,0 +1,14 @@
+age = 21
+
+if age > 18:
+    print("Совершеннолетний")
+elif age == 18:
+    print("Поздравляю вам 18")
+else:
+    print("Не совершеннолетний")
+
+count = 10
+
+while count > 0:
+    print(f"Send Email {count}")
+    count -= 1
