0a8e84759568941d2b91efa6cf61f31993b2ccc7
Kabrel <37102421+Kabrel@users.noreply.github.com>
2021-04-16 21:50:21 +0300

    Ex. 3 done

---
+++ Lesson1/HomeWork_3.py
@@ -0,0 +1,10 @@
+# Программа третьего домашнего задания
+
+input_n = input("Введите число n: ")
+
+n = int(input_n)
+nn = int(input_n + input_n)
+nnn = int(input_n + input_n + input_n)
+sum_n = n + nn + nnn
+
+print(f"Сумма чисел = {sum_n}")
